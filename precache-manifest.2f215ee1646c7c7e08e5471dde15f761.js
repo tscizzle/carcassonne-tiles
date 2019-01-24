6a2a2a7a949cd9a3991108369fa33c3fd75c53c1
Tyler S-C <tscizzle@gmail.com>
2019-01-24 00:19:18 -0500

    Updates

---
+++ precache-manifest.2f215ee1646c7c7e08e5471dde15f761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eccce76d527084fe03be",
+    "url": "/carcassonne-tiles/static/css/main.3ed2cc51.chunk.css"
+  },
+  {
+    "revision": "eccce76d527084fe03be",
+    "url": "/carcassonne-tiles/static/js/main.eccce76d.chunk.js"
+  },
+  {
+    "revision": "31e515dddd46c8de496d",
+    "url": "/carcassonne-tiles/static/js/1.31e515dd.chunk.js"
+  },
+  {
+    "revision": "81e68a863f4e39ec0629",
+    "url": "/carcassonne-tiles/static/js/runtime~main.81e68a86.js"
+  },
+  {
+    "revision": "19dedb7a535af5f5dada0a0c6dc25ff0",
+    "url": "/carcassonne-tiles/index.html"
+  }
+];
